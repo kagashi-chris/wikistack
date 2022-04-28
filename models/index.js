ecf2a60a782fe41075c98da54761a0cab81c5cf2
Chris Zhen <chris.zhenartist@gmail.com>
2022-04-28 17:03:30 -0400

    changes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack', {logging:false});
-
+const Sequelize = require("sequelize");
+const db = new Sequelize("wikistack", "postgres", "postgres", {
+  host: "localhost",
+  dialect: "postgres",
+  logging: false,
+});
 
 const Page = db.define("page", {
   title: { type: Sequelize.STRING, allowNull: false, defaultValue: "none" },
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const { addPage }= require('../views/')
+const express = require("express");
+const router = express.Router();
+const { addPage } = require("../views/");
 
-router.get('/', async(req, res, next)=>{
-    res.send('wiki.js')
-})
+router.get("/", async (req, res, next) => {
+  res.send("wiki.js");
+});
 
-router.post('/', async(req, res, next)=>{
-     const json = await res.json(req.body)
-      res.send(json)
-})
+router.get("/add", async (req, res, next) => {
+  res.send(addPage());
+});
 
-router.get('/add', async(req, res, next)=>{
-    
-    res.send(addPage())
-})
+router.post("/", async (req, res, next) => {
+  try {
+    res.send();
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
